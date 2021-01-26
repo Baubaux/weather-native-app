c54620144455838c7173d9be112b23390e92b39f
Baux Aurélien <baux.aurelien62@gmail.com>
2021-01-26 13:34:53 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { 
+  FlatList, 
+  StyleSheet, 
+  Text, 
+  View } from 'react-native';
+import TodayWeather from './components/TodayWeather';
+import axios from 'axios';
 
 export default function App() {
+
+  useEffect(() => { 
+    axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=50.4291723&lon=2.8319805&units=metric&lang=fr&exclude=minutely%2Chourly%2Calerts&appid=8c3a54c385c9c9d874d88f2cd6b3dda8&fbclid=IwAR03TiFxJLLNZiBoNQwegyiTkexqI7d3KjZHcFGnIUk1xTKk3lKWKzveesw')
+    .then(res => {
+      console.log(res.data);
+    })
+  }, [])
+
+  const [today, setToday] = useState([])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Bienvenu sur mon application méteo</Text>
+      <TodayWeather></TodayWeather>
+      <FlatList
+          data={today}
+          renderItem={({ item }) => {
+            return <TodayWeather item={item}/>
+          }}
+          keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 }
---
+++ components/TodayWeather.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const TodayWeather = () => {
+    return (
+        <View>
+            <Text>La météo actuelle :</Text>
+            <Text>La température actuelle est de :</Text>
+        </View>
+    )
+}
+
+export default TodayWeather
